7bd934bc0050e24ed7cae1585bd81a2f0e447101
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-02-10 16:01:30 -0600

    Arreglar return

--- src/lab4p2_equipo4/Peon.java
+++ src/lab4p2_equipo4/Peon.java
@@ -13,14 +13,15 @@ public class Peon extends Pieza {
     
 
     public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco){
-        if(y == 1 && esblanco == true){
+        if(y == 1 && esblanco == true){//PEONES BLANCOS
             
         }
-        if (y == 6 && esblanco == false ) {
+        if (y == 6 && esblanco == false ) {//PEONES NEGROS
             
         }
-        if (y != 1  && esblanco == true || y != 6 && esblanco == false) {
+        if (y != 1  && esblanco == true || y != 6 && esblanco == false) {//Proximos movimientos
             
         }
+     
     }
 }
