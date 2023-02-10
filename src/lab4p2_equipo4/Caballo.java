21a2c6ddc4836ae4422c1563c0dbb29a2d6ab607
JRafaelLobo <115841864+JRafaelLobo@users.noreply.github.com>
2023-02-10 14:38:58 -0600

    trabajando en el area correcta

--- src/lab4p2_equipo4/Caballo.java
+++ src/lab4p2_equipo4/Caballo.java
@@ -1,13 +1,12 @@
-
 package lab4p2_equipo4;
 
 public class Caballo {
 
-    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco) {
+    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco, Object tablero) {
         boolean temp = true;
         int distanciax = Math.abs(x - finalx);
         int distanciay = Math.abs(y - finaly);
-        if (tablero[finalx][finaly].getEsBlanco == esblanco) {
+        if (((Pieza) tablero[finalx][finaly]).getEsBlanco() == esblanco) {
             if (distanciax == 1 && distanciay == 2 || distanciax == 2 && distanciay == 1) {
             } else {
                 temp = false;
