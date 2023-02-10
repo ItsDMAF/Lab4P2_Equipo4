fc512f3be22ae1325240a60d6d6ef0be003d10c2
JRafaelLobo <115841864+JRafaelLobo@users.noreply.github.com>
2023-02-10 14:44:46 -0600

    Solucion matriz

--- src/lab4p2_equipo4/Torre.java
+++ src/lab4p2_equipo4/Torre.java
@@ -1,13 +1,13 @@
 package lab4p2_equipo4;
 
-public class Caballo extends Pieza {
+public class Torre extends Pieza {
 
     public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco, Object[][] tablero) {
         boolean temp = true;
         int distanciax = Math.abs(x - finalx);
         int distanciay = Math.abs(y - finaly);
         if (((Pieza) tablero[finalx][finaly]).getEsBlanco() == esblanco) {
-            if (distanciax == 1 && distanciay == 2 || distanciax == 2 && distanciay == 1) {
+            if (distanciax == x || distanciay == y) {
             } else {
                 temp = false;
             }
@@ -15,6 +15,16 @@ public class Caballo extends Pieza {
             temp = false;
         }
 
+        return temp;
+    }
+
+    private boolean testmiddlepiece(int x, int y, int finalx, int finaly) {
+        boolean temp=true;
+        if (x != finalx) {
+            
+        } else {
+
+        }
         return temp;
     }
 }
