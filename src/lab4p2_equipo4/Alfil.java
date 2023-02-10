bd88ebedb452f92a03cb163c35443ca3aa0b873c
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-02-10 15:17:07 -0600

    Alfil completo en teoria

--- src/lab4p2_equipo4/Alfil.java
+++ src/lab4p2_equipo4/Alfil.java
@@ -2,15 +2,60 @@ package lab4p2_equipo4;
 
 public class Alfil {
 
-    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblaco) {
+    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco, Object[][] tablero) {
         boolean temp = true;
         int xdistancia = Math.abs(x - finalx);
         int ydistancia = Math.abs(y - finaly);
-        if (tablero[finalx][finaly].getEsBlanco == esblanco) {
-        if (xdistancia == ydistancia) {
-            
+        if (((Pieza) tablero[finalx][finaly]).getEsBlanco() == esblanco) {
+            if (xdistancia == ydistancia) {
+
+            } else {
+                temp = false;
+            }
+        } else {
+            temp = false;
         }
-        } 
+
+        return temp;
+    }
+
+    private boolean testmiddlepiece(int x, int y, int finalx, int finaly, Object[][] tablero) {
+        boolean temp = true;
+        if (finalx < x) { //x --
+            if (finaly > x) {//y ++
+                for (int i = x - 1, j = y + 1; i < finalx; i--, j++) {
+                    if (tablero[i][j] instanceof Pieza) {
+                        return temp = false;
+                    }
+                }
+            } else {//y --
+                for (int i = x - 1, j = y - 1; i < finalx; i--, j--) {
+                    if (tablero[i][j] instanceof Pieza) {
+                        return temp = false;
+                    }
+                }
+
+            }
+
         }
+        if (finalx > x) {//x ++
+            if (finaly > x) {//y ++
+                for (int i = x + 1, j = y + 1; i < finalx; i++, j++) {
+                    if (tablero[i][j] instanceof Pieza) {
+                        return temp = false;
+                    }
+                }
+
+            } else {//y --
+                for (int i = x + 1, j = y - 1; i < finalx; i++, j--) {
+                    if (tablero[i][j] instanceof Pieza) {
+                        return temp = false;
+                    }
+                }
+
+            }
+        }
+
+        return temp;
     }
 }
