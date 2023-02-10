2a8f17c674a27f461f5f289a8617ea8ab4c3016f
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-02-10 16:29:21 -0600

    Peon mejora

--- src/lab4p2_equipo4/Peon.java
+++ src/lab4p2_equipo4/Peon.java
@@ -1,4 +1,3 @@
-
 package lab4p2_equipo4;
 
 public class Peon extends Pieza {
@@ -10,18 +9,28 @@ public class Peon extends Pieza {
     public Peon() {
         super();
     }
-    
 
-    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco){
-        if(y == 1 && esblanco == true){//PEONES BLANCOS
-            
+    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco) {
+        boolean temp = true;
+        int distanciay = Math.abs(y - finaly);
+        if (y == 1 && esblanco == true) {//PEONES BLANCOS
+            if (distanciay >= 2 && distanciay > y) {
+            } else {
+                temp = false;
+            }
         }
-        if (y == 6 && esblanco == false ) {//PEONES NEGROS
-            
+        if (y == 6 && esblanco == false) {//PEONES NEGROS
+            if (distanciay <= 2 && distanciay < y) {
+            } else {
+                temp = false;
+            }
         }
-        if (y != 1  && esblanco == true || y != 6 && esblanco == false) {//Proximos movimientos
-            
+        if (y != 1 && esblanco == true || y != 6 && esblanco == false) {//Proximos movimientos
+            if (distanciay <= 1) {
+            } else {
+                temp = false;
+            }
         }
-     
+        return temp;
     }
 }
