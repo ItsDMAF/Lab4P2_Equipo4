ef03ddb11f788ef52788305e702207b8333eb726
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-02-10 17:23:22 -0600

    Peon yes

--- src/lab4p2_equipo4/Peon.java
+++ src/lab4p2_equipo4/Peon.java
@@ -10,27 +10,47 @@ public class Peon extends Pieza {
         super();
     }
 
-    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco) {
+    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco, Object[][] tablero) {
         boolean temp = true;
         int distanciay = Math.abs(y - finaly);
-        if (y == 1 && esblanco == true) {//PEONES BLANCOS
-            if (distanciay >= 2 && distanciay > y) {
-            } else {
-                temp = false;
+
+        if (((Pieza) tablero[finalx][finaly]).getEsBlanco() == esblanco) {
+
+            if (y == 1 && esblanco == true) {//PEONES BLANCOS
+                if (distanciay >= 2 && distanciay > y) {
+                    if (tablero[x][y-1] instanceof  Pieza) {
+                        
+                    }
+                } else {
+                    temp = false;
+                }
+            }
+            if (y == 6 && esblanco == false) {//PEONES NEGROS
+                if (distanciay <= 2 && distanciay < y) {
+                    if (tablero[x][y+1] instanceof  Pieza) {
+                        
+                    }
+                } else {
+                    temp = false;
+                }
+            }
+//------------------------------------------------------------------------------
+            if (y != 1 && esblanco == true || y != 6 && esblanco == false) {//Proximos movimientos
+                if (distanciay <= 1) {
+                } else {
+                    temp = false;
+                }
+            }
+            if (esblanco == false && finaly == y - 1) {//Comer lado blanco
+                temp = true;
+
+            }
+            if (esblanco == true && finaly == y + 1) {//Comer lado negro
+                temp = true;
+
             }
         }
-        if (y == 6 && esblanco == false) {//PEONES NEGROS
-            if (distanciay <= 2 && distanciay < y) {
-            } else {
-                temp = false;
-            }
-        }
-        if (y != 1 && esblanco == true || y != 6 && esblanco == false) {//Proximos movimientos
-            if (distanciay <= 1) {
-            } else {
-                temp = false;
-            }
-        }
+
         return temp;
     }
 
