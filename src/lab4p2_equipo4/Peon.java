4d040fb89119ca0a10628c77ecb79c0123b11eab
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-02-10 15:35:35 -0600

    Constructores integrados

--- src/lab4p2_equipo4/Peon.java
+++ src/lab4p2_equipo4/Peon.java
@@ -3,8 +3,24 @@ package lab4p2_equipo4;
 
 public class Peon extends Pieza {
 
+    public Peon(boolean esBlanco, int x, int y, int finalx, int finaly) {
+        super(esBlanco, x, y, finalx, finaly);
+    }
+
+    public Peon() {
+        super();
+    }
     
-    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblaco){
-        
+
+    public boolean movimiento(int x, int y, int finalx, int finaly, boolean esblanco){
+        if(y == 1 && esblanco == true){
+            
+        }
+        if (y == 6 && esblanco == false ) {
+            
+        }
+        if (y != 1  && esblaco == true || y != 6 && esblaco == false) {
+            
+        }
     }
 }
